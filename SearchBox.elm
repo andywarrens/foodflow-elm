959f434046be314706a2538e2169fd2bca14248d
Andy Warrens <andy.warrens@telenet.be>
2018-03-30 00:07:06 +0200

    SearchBox can search & select an ingredient

--- SearchBox.elm
+++ SearchBox.elm
@@ -5,7 +5,7 @@ import Ingredient exposing (Ingredient, turkey, rijst, pezo)
 import Html exposing (Html, Attribute, div, img, ul, li, input, text, node)
 import Element exposing (show)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 import Http
 import Json.Decode as Decode
 
@@ -24,13 +24,15 @@ main =
 type alias Model =
   { search      : String
   , ingredientsUrls : List String
-  , ingredients : List Ingredient }
+  , ingredients : List Ingredient
+  , selectedIngredient : Maybe Ingredient }
 
 defaultModel : Model
 defaultModel =
-  { search         = ""
-  , ingredientsUrls = [ "" ]
-  , ingredients     = [ turkey, rijst, pezo ] }
+  { search          = ""
+  , ingredientsUrls = []
+  , ingredients     = [ turkey, rijst, pezo ]
+  , selectedIngredient = Nothing }
 
 init : String -> (Model, Cmd Msg)
 init topic = (defaultModel, fetchImages topic)
@@ -38,10 +40,23 @@ init topic = (defaultModel, fetchImages topic)
 toHtml : Ingredient -> Html Msg
 toHtml ing = img [ src ing.img, alt ing.name ] []
 
+ingredientUrlToHtml : String -> Maybe Ingredient -> Url -> Html Msg
+ingredientUrlToHtml searchTopic selected url = 
+    let className = case selected of
+        Just ingr -> if (ingr.img == url) then "selected-image" else ""
+        Nothing   -> ""
+    in img [ src url
+           , alt searchTopic
+           , class className
+           , onClick (SelectIngredient url)
+           ] []
+
 -- UPDATE
 
+type alias Url = String
 type Msg = TextInput String
          | NewImages (Result Http.Error (List String))
+         | SelectIngredient Url
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -55,15 +70,20 @@ update msg model =
     NewImages (Err _) ->
       ({ model | ingredientsUrls = ["error"] }, Cmd.none)
 
+    SelectIngredient url ->
+        let ingr = Ingredient model.search 1 "piece" url
+        in ({ model | selectedIngredient = Just ingr }, Cmd.none)
+
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ stylesheet
+  div [ class "container" ]
+    [ stylesheet "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+    , stylesheet "css/main.css"
     , input [ placeholder "Search for ingredients", onInput TextInput ] []
-    , ul [] (List.map (\a -> img [src a, alt a] []) model.ingredientsUrls)
+    , ul [] (List.map (ingredientUrlToHtml model.search model.selectedIngredient) model.ingredientsUrls)
     , ul [] (List.map toHtml model.ingredients)
     ]
 
@@ -91,14 +111,13 @@ decodeUrls =
 
 
 -- CSS
-stylesheet =
+stylesheet url =
     let
         tag = "link"
         attrs =
             [ attribute "rel"       "stylesheet"
             , attribute "property"  "stylesheet"
-            --, attribute "href"      "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
-            , attribute "href"      "css/main.css"
+            , attribute "href" url 
             ]
         children = []
     in 
